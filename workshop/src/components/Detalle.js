a8e407156b00e7d8f5d676efc4401f6021fffbbb
Andreina Rodriguez <andreaguerrero.ar@gmail.com>
2022-04-03 10:46:37 -0500

    se suebn cambios para que se pueda observar el detalle

--- workshop/src/components/Detalle.js
+++ workshop/src/components/Detalle.js
@@ -1,48 +1,57 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import { url } from "../url/url";
-import Cards from './Cards';
+import axios from "axios";
+import "../styles/Cards.css";
 
 
-const Detalle = async () => {
 
-    const [detalle, setDetalle] = useState([])
+const Detalle = () => {
 
+    const [data, setData] = useState([]);
 
-    const resp = await fetch(url)
-    const dato = await resp.json()
+    const getData = () => {
+        axios
+            .get(url)
+            .then((resp) => setData(resp.data))
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+    useEffect((e) => {
+        getData();
+    }, []);
 
-    console.log(dato);
-
-    let Id = Number(JSON.parse(localStorage.getItem('Id')))
-    const cardDetalle = dato.filter((element) => (element.id == Id));
-
-    setDetalle(cardDetalle)
-
+    let Id = JSON.parse(localStorage.getItem('Id'))
+    const cardDetalle = data.filter(element => element.id == Id);
 
     return (
         <div className="container-card1">
+            {
+                cardDetalle.map(e => (
+                    <div className="card-item1" key={e.id}>
+                        <div className="card-header1">
+                            <h2 className="card-title1">{e.title}</h2>
+                        </div>
+                        <p className="card-descripcion1">{e.description}</p>
+                        {/* <div className="card-speakers1">
+                            <h4 className="speaker1">{e.speakers[0]}</h4>
+                            <h4 className="speaker1">{e.speakers[1]}</h4>
+                            <h4 className="speaker1">{e.speakers[2]}</h4>
+                        </div> */}
+                        <h4 className="card-SegmentIndustry1">
+                            Segmento industrial: <span>{e.industry_segment}</span>
+                        </h4>
+                        <h4 className="card-mainTopic1">
+                            Tema principal: <span>{e.primary_topic}</span>
+                        </h4>
+                        <br></br>
 
-            <div className="card-item1" key={detalle[0].id}>
-                <div className="card-header1">
-                    <h2 className="card-title1">{detalle[0].title}</h2>
-                </div>
-                <p className="card-descripcion1">{detalle[0].description}</p>
-                <div className="card-speakers1">
-                    <h4 className="speaker1">{detalle[0].speakers[0]}</h4>
-                    <h4 className="speaker1">{detalle[0].speakers[1]}</h4>
-                    <h4 className="speaker1">{detalle[0].speakers[2]}</h4>
-                </div>
-                <h4 className="card-SegmentIndustry1">
-                    Segmento industrial: <span>{detalle[0].industry_segment}</span>
-                </h4>
-                <h4 className="card-mainTopic1">
-                    Tema principal: <span>{detalle[0].primary_topic}</span>
-                </h4>
-                <br></br>
+                        <Link to='/'>Regresar</Link>
+                    </div>
+                ))
+            }
 
-                <Link to='/'>Regresar</Link>
-            </div>
 
 
         </div>
